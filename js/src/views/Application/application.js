409220ff4270365646469d3081749772411ec462
Nicolas Gotchac <ngotchac@gmail.com>
2016-12-15 01:31:04 +0100

    linting

--- js/src/views/Application/application.js
+++ js/src/views/Application/application.js
@@ -16,7 +16,6 @@
 
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { observer } from 'mobx-react';
 
 import Connection from '../Connection';
